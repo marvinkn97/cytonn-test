76b940fec9335ae6ee469d6ce42bf5714d30f752
Marvin Nyingi <nyingi.marvin@outlook.com>
2023-06-22 21:59:22 +0300

    first commit

---
+++ src/main/java/dev/marvin/ticketbooking/TicketBookingApplication.java
@@ -0,0 +1,13 @@
+package dev.marvin.ticketbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketBookingApplication.class, args);
+	}
+
+}
